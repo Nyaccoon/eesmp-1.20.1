0c9d1ef7c3da104c2ea5158ecc4f44d000afdfc4
Marceline <nyaccoon@gmail.com>
2026-09-26 00:03:10 +0200

    finished functionality of reality and fixed heart of zuelia

--- src/main/java/com/nyaccoon/eesmp/item/customItems/HeartOfReality.java
+++ src/main/java/com/nyaccoon/eesmp/item/customItems/HeartOfReality.java
@@ -2,12 +2,19 @@ package com.nyaccoon.eesmp.item.customItems;
 
 import java.util.List;
 
+import com.nyaccoon.eesmp.item.ModItems;
+
 import net.minecraft.client.item.TooltipContext;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
 import net.minecraft.text.Style;
 import net.minecraft.text.Text;
 import net.minecraft.text.TextColor;
+import net.minecraft.util.Hand;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
 
 public class HeartOfReality extends Item {
@@ -17,5 +24,11 @@ public class HeartOfReality extends Item {
     @Override
     public void appendTooltip(ItemStack stack, World world, List<Text> tooltip, TooltipContext context) {
         tooltip.add(Text.translatable("tooltip.eesmp.heart_of_reality").setStyle(Style.EMPTY.withColor(TextColor.fromRgb(0xFED89D))));
+    }
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        user.getItemCooldownManager().set(this,600);
+        ItemStack tempItem = new ItemStack(Registries.ITEM.get(Identifier.of("origins","orb_of_origin")));
+        user.getInventory().insertStack(tempItem);
+        return TypedActionResult.success(new ItemStack(ModItems.HEART_OF_REALITY),world.isClient());
     }
 }
--- src/main/java/com/nyaccoon/eesmp/item/customItems/HeartOfZuelia.java
+++ src/main/java/com/nyaccoon/eesmp/item/customItems/HeartOfZuelia.java
@@ -2,16 +2,24 @@ package com.nyaccoon.eesmp.item.customItems;
 
 import java.util.List;
 
+import com.nyaccoon.eesmp.item.ModItems;
+
 import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.thrown.EnderPearlEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.stat.Stats;
 import net.minecraft.text.Style;
 import net.minecraft.text.Text;
 import net.minecraft.text.TextColor;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
 
 public class HeartOfZuelia extends Item {
@@ -30,4 +38,21 @@ public class HeartOfZuelia extends Item {
             if(selected || player.getOffHandStack().isOf(this)) player.addStatusEffect(new StatusEffectInstance(StatusEffect.byRawId(1),1,1), player);
         }
     }
+
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+		ItemStack itemStack = new ItemStack(ModItems.SHARD_OF_SPACE); // creates a new ItemStack instance of the user's itemStack in-hand
+		world.playSound(null, user.getX(), user.getY(), user.getZ(), SoundEvents.ENTITY_SNOWBALL_THROW, SoundCategory.NEUTRAL, 0.5F, 1F); // plays a globalSoundEvent
+		
+        user.getItemCooldownManager().set(this,200);
+
+		if (!world.isClient) {
+			EnderPearlEntity pearl = new EnderPearlEntity(world, user);
+            pearl.setItem(itemStack);
+            pearl.setVelocity(user,user.getPitch(),user.getYaw(),0.0f,2.25f,0.0f);
+            world.spawnEntity(pearl);
+		}
+ 
+		user.incrementStat(Stats.USED.getOrCreateStat(this));
+		return TypedActionResult.success(new ItemStack(ModItems.HEART_OF_ZUELIA), world.isClient());
+	}
 }
